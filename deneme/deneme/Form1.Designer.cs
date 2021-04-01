bbcc2878488dc39cedacc753555ab2a14c03ca97
mali2005 <81029491+mali2005@users.noreply.github.com>
2021-04-01 18:05:35 +0300

    Add files via upload
    Müzik indirmek için bir program. C# ile yapıldı.

---
+++ deneme/deneme/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace deneme
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Desktop;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(1, 1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(76, 24);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "İNDİR";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.Desktop;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button2.Location = new System.Drawing.Point(354, 1);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(76, 24);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "ÇIKIŞ";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.textBox1.Location = new System.Drawing.Point(-1, 31);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(431, 23);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textBox1_MouseClick);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.GrayText;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(136, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(212, 24);
+            this.panel1.TabIndex = 3;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.Info;
+            this.label1.Location = new System.Drawing.Point(0, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(188, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Formatı seçin. Buradan sürükleyin.";
+            this.label1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label1_MouseDown);
+            this.label1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.label1_MouseUp);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(81, 2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(49, 23);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(430, 60);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ deneme/deneme/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using MediaToolkit.Model;
+using MediaToolkit;
+using VideoLibrary;
+
+namespace deneme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int farkx, farky;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.TransparencyKey = this.BackColor;
+            comboBox1.Items.Add(".mp3");
+            comboBox1.Items.Add(".mp4");
+            comboBox1.Items.Add(".wav");
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.Location = new Point(Cursor.Position.X - farkx, Cursor.Position.Y - farky);
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            farkx = Cursor.Position.X - this.Location.X;
+            farky = Cursor.Position.Y - this.Location.Y;
+            timer1.Start();
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void label1_MouseDown(object sender, MouseEventArgs e)
+        {
+            farkx = Cursor.Position.X - this.Location.X;
+            farky = Cursor.Position.Y - this.Location.Y;
+            timer1.Start();
+        }
+
+        private void label1_MouseUp(object sender, MouseEventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+                folderDlg.ShowNewFolderButton = true;
+                // Show the FolderBrowserDialog.  
+                DialogResult result = folderDlg.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    WebClient web = new WebClient();
+                    string url = web.DownloadString("https://www.google.com/search?q=" + textBox1.Text.Trim().Replace(' ', '+'));
+                    int a = url.IndexOf("watch?v=");
+                    string kod = "https://www.youtube.com/watch?v=" + url.Substring(a + 8, 11);
+
+                    YouTube y = new YouTube();
+                    Video vid = y.GetVideo(kod);
+
+                    if (comboBox1.Text == ".mp4")
+                    {
+                        System.IO.File.WriteAllBytes(folderDlg.SelectedPath + "//" + vid.FullName, vid.GetBytes());
+                    }
+                    else
+                    {
+                        System.IO.File.WriteAllBytes(folderDlg.SelectedPath + "//" + vid.FullName, vid.GetBytes());
+                        var inputFile = new MediaFile { Filename = folderDlg.SelectedPath + "//" + vid.FullName };
+                        var outputFile = new MediaFile { Filename = $"{folderDlg.SelectedPath + "//" + vid.FullName + comboBox1.Text}" };
+
+                        using (var engine = new Engine())
+                        {
+                            engine.GetMetadata(inputFile);
+                            engine.Convert(inputFile, outputFile);
+                        }
+                        System.IO.File.Delete(folderDlg.SelectedPath + "//" + vid.FullName);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Girdiğinizin bir şarkı olduğundan emin olun.","HATA",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+        }
+
+        private void textBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+
+        }
+    }
+}
